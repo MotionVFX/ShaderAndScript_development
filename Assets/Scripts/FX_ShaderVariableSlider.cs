157cff0cda07495d99988dbfb360b2bb7042b262
a.kagarmanov <a.kagarmanov@corp.nekki.ru>
2020-01-26 19:43:24 +0500

    FX_ShaderVariableSlider

---
+++ Assets/Scripts/FX_ShaderVariableSlider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class FX_ShaderVariableSlider : MonoBehaviour
+{
+    public bool isEditor = false;
+    public GameObject obj;
+    [Range(0, 1)]
+    public float value = 1;
+    public string shaderVarName;
+
+    private int i;
+    private Renderer[] rends;
+
+    void Update()
+    {
+        if (Application.isPlaying) TransmitToShader();
+        if (isEditor) TransmitToShaderEditor();
+    }
+
+    void TransmitToShader()
+    {
+        rends = obj.GetComponentsInChildren<Renderer>();
+        for (i = 0; i < (rends.Length); i++)
+            rends[i].material.SetFloat(shaderVarName, value);
+    }
+
+    void TransmitToShaderEditor()
+    {
+        rends = obj.GetComponentsInChildren<Renderer>();
+        for (i = 0; i < (rends.Length); i++)
+            rends[i].sharedMaterial.SetFloat(shaderVarName, value);
+    }
+}
